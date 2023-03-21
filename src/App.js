00c80bca97fcd3f27b706ab40ae1db0dda220de5
Aditya <95631458+Sharmaaditya05@users.noreply.github.com>
2023-03-21 21:27:15 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from "./MyComponents/Header";
+import ScholarShip from "./MyComponents/ScholarShip";
+import ExamRegestration from "./MyComponents/ExamRegestration";
+import SportsRegestration from "./MyComponents/SportsRegestration";
+import PayBills from "./MyComponents/PayBills";
+import TakeInsurance from "./MyComponents/TakeInsurance";
+import BookTickets from "./MyComponents/BookTickets";
+import GetMoreHelp from "./MyComponents/GetMoreHelp";
+import LatestNews from "./MyComponents/LatestNews";
+import LogIn from "./MyComponents/LogIn";
+import SignIn from "./MyComponents/SignIn";
+import Side from "./MyComponents/Side";
+import Footer from "./MyComponents/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <ScholarShip />
+      {/* <ExamRegestration /> */}
+      {/* <SportsRegestration /> */}
+      {/* <PayBills /> */}
+      {/* <TakeInsurance /> */}
+      {/* <BookTickets/> */}
+      {/* <GetMoreHelp/> */}
+      {/* <LatestNews/> */}
+      {/* <LogIn/> */}
+      {/* <SignIn /> */}
+      {/* <Side/> */}
+      <Footer/>
     </div>
   );
 }
---
+++ src/MyComponents/BookTickets.js
@@ -0,0 +1,38 @@
+export default function BookTickets () {
+    let myStyle = {
+        // margin: "100px 30px"
+
+    }
+    return (
+        <div style={myStyle} >
+            <h1>About the Book Tickets</h1>
+            <div className="Flight">
+                <h2>Flight Tickets</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero.
+                    Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?</p>
+                <button className="btn.sm">Book Ticket</button>
+            </div>
+
+            <div className="Train">
+                <h2>Train Tickets</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero.
+                    Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?</p>
+                <button className="btn.sm">Book Ticket</button>
+            </div>
+
+            <div className="Bus">
+                <h2>Bus Tickets</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero.
+                    Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?</p>
+                <button className="btn.sm">Book Ticket</button>
+            </div>
+
+            <div className="Hotel">
+                <h2>Hotel Booking</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero.
+                    Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?</p>
+                <button className="btn.sm">Book Hotel</button>
+            </div>
+        </div>
+    )
+}
