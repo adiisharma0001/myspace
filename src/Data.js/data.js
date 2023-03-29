977201c5821776bf17982ab23416e03fefd27436
Aditya <95631458+Sharmaaditya05@users.noreply.github.com>
2023-03-29 21:41:37 +0530

    update

---
+++ src/Data.js/data.js
@@ -0,0 +1,136 @@
+export const scholarships = [
+    {
+        title: "Private Scholarships",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "State Scholarships",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "National Scholarships",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    }
+];
+
+export const examregestration = [
+    {
+        title: "Under Graduate Admission Form",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+ 
+    },
+    {
+        title: "Post Graduate Admission Form",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "Internship",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "Full time Job",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    }
+];
+
+export const sportsregestration = [
+    {
+        title: "Participate in College Sports fest Events",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "District Sports Event",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "State Sports Events",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "National Sports Events",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+
+    }
+];
+
+export const paybills = [
+    {
+        title: "Electricity Bill",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "House Bill",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "Water Bill",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "Phone Bill",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+
+    }
+];
+
+export const booktickets = [
+    {
+        title: "Flight Tickets",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "Train Tickets",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "Bus Tickets",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "Hotel Booking",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+
+    }
+];
+
+
+export const takeinsurance = [
+    {
+        title: "Health Insurance",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "Bike Insurance",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "Car Insurance",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "Property Insurance",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+
+    }
+];
+
+
+export const latestnews = [
+    {
+        title: "News Regarding Loan",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "News Regarding Jobs",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "News Regarding Scholar Ship",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+    },
+    {
+        title: "News Regarding Exams",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi tenetur fugit sunt quisquam dolorem pariatur repellat obcaecati reiciendis enim dolor quod, porro iusto tempore sequi. Delectus at quidem libero. Iusto mollitia voluptatum eos sunt, ullam commodi, laboriosam optio fugit voluptate totam impedit. Necessitatibus, asperiores. Tempore eligendi consectetur repudiandae mollitia quisquam? Eligendi sequi earum necessitatibus? Illo exercitationem est a veniam?"
+
+    }
+];
